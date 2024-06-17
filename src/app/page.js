f1f81aad85cc1fcc16e9009fa28a124dcb500f0a
Frengky Manjo <frengkysimanjorang29@gmail.com>
2024-06-17 18:34:44 +0700

    make notetakingapp

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
-import { CardForm } from "@/components/cardForm";
+import { NoteCard } from "@/components/noteCard";
+import { NoteForm } from "@/components/noteForm";
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch ("https://v1.appbackend.io/v1/rows/IbffA4phDHSs", {
+    cache: "no-store",
+  });
+  const {data} = await res.json();
+
   return (
-    <main>
-      <CardForm />
-    </main>
+  <main className="max-w-2xl m-auto py-12space-y-4">
+    <h1>All my notes</h1>
+    <div className="space-y-4">
+      {data.map((item) => {
+      return (
+        <NoteCard key={item._id} item={item} />
+      );
+    })}
+    </div>
+    <NoteForm />
+  </main>
   );
 };

238a8bee55c319f4caa988010e59e27af3809035
Frengky Manjo <frengkysimanjorang29@gmail.com>
2024-06-17 16:01:54 +0700

    add notes

--- src/components/cardForm.jsx
+++ src/components/cardForm.jsx
@@ -1,24 +1,39 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const CardForm = () => {
-    const [counter, setCounter] = useState(0); //rerender
+    const [note, setNote] = useState("");
+    const [notes, setNotes] = useState([]);
 
-    function handleIncrement(){
-        setCounter((currentValue) => currentValue + 1);
-    };
+    function handleAddNote(){
+        const newNotes = [...notes]; //cloning dari data notes ke newNotes
+        newNotes.push(note) // ditambah new note
+        setNotes(newNotes) // set ke State
+        localStorage.setItem("notes", JSON.stringify(newNotes)); // disimpan ke local storage
+        setNote("");
+        
 
-
-    function handleDecrement(){
-        setCounter((currentValue) => currentValue - 1)
     }
+
+    //dijalankan sekali ketika component di mount
+    useEffect(() => {
+        const existingData = localStorage.getItem("notes") //diambil dari local storage
+        if(existingData){
+            const parseData = JSON.parse(existingData)// parsing string jadi array
+            setNotes(parseData);
+        }
+    }, [notes])
 
   return (
     <div>
-        <div>Number: {counter}</div>
-        <button onClick={handleIncrement}>Increment</button>
-        <button onClick={handleDecrement}>Decrement</button>
+        <input value={note} onChange={(evet) => setNote(event.target.value)} />
+        <button onClick={handleAddNote}>Add to Notes</button>
+        <section>
+            {notes.map((item, index) => {
+                return <div key={index}>{item}</div>
+            })}
+        </section>
     </div>
   );
 };
